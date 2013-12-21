76322ebe75b33df08ba140dbd6cb8a9b37967465
Michal Srch <michalsrch@gmail.com>
2013-12-21 22:27:30 +0000

    Adding new pattern function.

--- src/extractor.js
+++ src/extractor.js
@@ -45,6 +45,10 @@
         return val && Object.prototype.toString.call(val) === '[object Object]';
     };
 
+    _.isRegExp = function isRegExp(val) {
+        return val && Object.prototype.toString.call(val) === '[object RegExp]';
+    };
+
     _.isNot = function isNot(result) {
         return !result;
     };
@@ -69,25 +73,38 @@
         }
     }
 
-    Extr.addPattern = function addPattern(name, regex) {
+    function buildPattern(regexp) {
 
-    };
+        return function findMatch(string) {
+            return (string + '').match(regexp) || [];
+        };
+    }
 
-    Extr.changePattern = function changePattern(name, regex) {
+    function addPattern(name, regexp) {
 
-    };
+        if (!_.isUndefined(patterns[name])) {
+            throw new Error('Extractor: Pattern already exists!');
+        }
 
-    Extr.removePattern = function removePattern(name) {
-
-    };
+        if (name && _.isRegExp(regexp)) {
+            patterns[name + ''] = buildPattern(regexp);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
     Extr.VERSION = '0.0.1';
+    Extr.addPattern = addPattern;
 
     Extr.noConflict = function noConflict() {
         root.Extractor = previousExtractor;
         return this;
     };
+
+    Extr._ = _;
+    Extr.patterns = patterns;
 
     return Extr;
 });
